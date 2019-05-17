3fba9f113d1612514718c928dfbc183d01b95491
RomanShprenger <shprengerroman@gmail.com>
2019-05-17 18:17:30 +0300

    Add to repo

---
+++ components/CommentList.js
@@ -0,0 +1,9 @@
+import Comment from './Comment.js';
+
+export default ({ comments }) => (
+  <React.Fragment>
+    {
+      comments.map(comment => <Comment key={comment.id} comment={comment} />)
+    }
+  </React.Fragment>
+)
